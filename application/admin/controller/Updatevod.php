e2a886bb4b8ebc9561092f44cae0ad7749768375
zjdboy <zjdboy@163.com>
2019-07-13 21:14:00 +0800

    zjdboy
    Update

--- application/admin/controller/Updatevod.php
+++ application/admin/controller/Updatevod.php
@@ -30,6 +30,7 @@ class Updatevod extends Base
         $data = [];
         $data['vod_id'] = $ret['info']['vod_id'];
         $data['type_id'] = $ret['info']['type_id'];
+        $data['type_id_1'] = $ret['info']['type_id_1'];
         $data['vod_name'] = $ret['info']['vod_name'];
         $data['vod_en'] = $ret['info']['vod_en'];
         $data['vod_status'] = $ret['info']['vod_status'];
