6d3ac271bc06e6028627ccb0c498848fd1674316
zjdboy <zjdboy@163.com>
2019-07-13 20:15:03 +0800

    update .gitignore

--- runtime/cache/32/c8ba10c68beebf010194bbc30c1748.php
+++
@@ -1,4 +0,0 @@
-<?php
-//000000003600
- exit();?>
-a:27:{s:7:"vod_all";i:180;s:7:"vod_min";d:2;s:7:"art_all";i:1;s:7:"art_min";d:5;s:10:"type_all_4";i:8;s:10:"type_all_0";i:181;s:11:"type_all_20";i:4;s:11:"type_all_22";i:15;s:11:"type_all_23";i:2;s:11:"type_all_24";i:4;s:11:"type_all_25";i:8;s:11:"type_all_26";i:2;s:11:"type_all_27";i:14;s:11:"type_all_28";i:10;s:11:"type_all_29";i:17;s:11:"type_all_30";i:1;s:11:"type_all_34";i:95;s:11:"type_all_31";i:1;s:9:"topic_all";i:10;s:11:"topic_today";i:0;s:9:"tpoic_min";d:1;s:9:"actor_all";i:0;s:11:"actor_today";i:0;s:9:"actor_min";d:0;s:8:"role_all";i:0;s:10:"role_today";i:0;s:8:"role_min";d:0;}
--- runtime/cache/4e/819c837d54a6ed09abc77a8560a66f.php
+++
@@ -1,4 +0,0 @@
-<?php
-//000000000000
- exit();?>
-a:1:{s:16:"admin_login_init";a:1:{i:0;s:33:"\addons\adminloginbg\Adminloginbg";}}
